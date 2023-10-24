d7212753be076827d0ffc2f2ccf29ab197717352
dipache <97771699+dipache@users.noreply.github.com>
2023-10-23 23:13:52 -0400

    Update app.py

--- ApiPractica2c/app.py
+++ ApiPractica2c/app.py
@@ -1,25 +1,14 @@
 from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cnp2_user:my_cool_secret_2@postgres-db:5432/cnp2_database'
+db = SQLAlchemy(app)
 
-
-directories = []
-def get_object(id):
-    for obj in directories:
-        if obj["id"] == id:
-            return obj
-    return None
-
-def validate_object(obj):
-    if not isinstance(obj, dict):
-        return False
-    if not "name" in obj or not isinstance(obj["name"], str):
-        return False
-    if not "emails" in obj or not isinstance(obj["emails"], list):
-        return False
-    for email in obj["emails"]:
-        if not isinstance(email, str):
-            return False
-    return True
+class Directory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    emails = db.Column(db.ARRAY(db.String(100)))
 
 @app.route("/status/", methods=["GET"])
 def status():
@@ -27,101 +16,39 @@ def status():
 
 @app.route("/directories/", methods=["GET"])
 def get_directories():
-
     page = int(request.args.get("page", 1))
     page_size = int(request.args.get("page_size", 10))
- 
+
     start = (page - 1) * page_size
     end = start + page_size
- 
-    results = directories[start:end]
+
+    directories = Directory.query.offset(start).limit(page_size).all()
 
     response = {
-        "count": len(directories),
-        "next": f"/directories/?page={page + 1}&page_size={page_size}" if end < len(directories) else None,
+        "count": Directory.query.count(),
+        "next": f"/directories/?page={page + 1}&page_size={page_size}" if end < Directory.query.count() else None,
         "previous": f"/directories/?page={page - 1}&page_size={page_size}" if start > 0 else None,
-        "results": results
+        "results": [
+            {"id": directory.id, "name": directory.name, "emails": directory.emails}
+            for directory in directories
+        ]
     }
     return jsonify(response)
 
-
 @app.route("/directories/", methods=["POST"])
-def create_object():
-   
-    data = request.get_json()
-  
-    if not validate_object(data):
-        abort(400) # Bad request
- 
-    data["id"] = len(directories) + 1
-   
-    directories.append(data)
-   
-    return jsonify(data), 201
-
-
-@app.route("/directories/<int:id>/", methods=["GET"])
-def get_object_by_id(id):
-
-    obj = get_object(id)
-
-    if not obj:
-        abort(404)
- 
-    return jsonify(obj)
-
-
-@app.route("/directories/<int:id>/", methods=["PUT"])
-def update_object_by_id(id):
-
-    obj = get_object(id)
-
-    if not obj:
-        abort(404)
-
+def create_directory():
     data = request.get_json()
 
     if not validate_object(data):
-        abort(400) # Bad request
+        abort(400)  # Bad request
 
-    obj["name"] = data["name"]
-    obj["emails"] = data["emails"]
-  
-    return jsonify(obj)
+    directory = Directory(name=data["name"], emails=data["emails"])
+    db.session.add(directory)
+    db.session.commit()
 
+    return jsonify({"id": directory.id, "name": directory.name, "emails": directory.emails}), 201
 
-@app.route("/directories/<int:id>/", methods=["PATCH"])
-def patch_object_by_id(id):
-  
-    obj = get_object(id)
-    
-    if not obj:
-        abort(404)
-    
-    data = request.get_json()
-   
-    if not isinstance(data, dict):
-        abort(400) # Bad request
+# Resto de las rutas...
 
-    if "name" in data and isinstance(data["name"], str):
-        obj["name"] = data["name"]
-    if "emails" in data and isinstance(data["emails"], list):
-        for email in data["emails"]:
-            if not isinstance(email, str):
-                abort(400) # Bad request
-        obj["emails"] = data["emails"]
-    
-    return jsonify(obj)
-
-# Ruta para eliminar un objeto por id
-@app.route("/directories/<int:id>/", methods=["DELETE"])
-def delete_object_by_id(id):
-  
-    obj = get_object(id)
- 
-    if not obj:
-        abort(404)
-  
-    directories.remove(obj)
- 
-    return "", 204
+if __name__ == "__main__":
+    app.run()
